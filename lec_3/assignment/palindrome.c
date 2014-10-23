1dde37599c00495c6b9f34671961de4019e1f4a5
rubberyuzu <0696338342@mail.ecc.u-tokyo.ac.jp>
2014-10-23 18:16:08 +0900

    added an assignment folder

---
+++ lec_3/assignment/palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+// to be implemented!
+int is_palindrome?(char str[]);
+
+// int main() {
+
+// }
+
+// int is_palindrome?(char str[]) {
+// 	int len = strlen(str[]);
+// 	for(i = 0; i < len/2 ; i++) {
+
+// 	}
+// }
