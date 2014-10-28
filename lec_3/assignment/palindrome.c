4848126a5a0f27ee3c878fc39ca9722fa9a0c923
rubberyuzu <yuzu0125@gmail.com>
2014-10-28 12:15:17 +0900

    made the palindrome work

--- lec_3/assignment/palindrome.c
+++ lec_3/assignment/palindrome.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-// to be implemented!
-int is_palindrome?(char str[]);
+int is_palindrome(char str[]);
+// this above is declaration of the function,
+// so that you can call it in the main funciton
+// even before you define it. (prototype declaration)
 
-// int main() {
+int main() {
+	char str[256];
+	// here setting the limit of the length of the string.
+	fgets(str, sizeof(str), stdin);
 
-// }
+	if(is_palindrome(str)){
+		printf("%s is palindrome.\n", str);
+	} else {
+		printf("%s is not palindrome.\n", str);
+	}
 
-// int is_palindrome?(char str[]) {
-// 	int len = strlen(str[]);
-// 	for(i = 0; i < len/2 ; i++) {
+	return 0;
+}
 
-// 	}
-// }
+int is_palindrome(char str[]) {
+	// note 
+	int len = strlen(str);
+
+
+	if(len <=2){
+		return 1;
+	}
+
+	int i;
+	for(i = 0; i < len/2 ; i++) {
+		if(str[i] != str[len-i-2]){
+			return 0;
+		}
+	}
+	return 1;
+}
