26840bdde86cffbf013a0bdc9b45861c311e58a7
rubberyuzu <yuzu0125@gmail.com>
2014-11-20 16:27:12 +0900

    strcpy comp

--- prep/strcpy1.c
+++ prep/strcpy1.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int strcpy1(char *s, char *t);
+void strcpy1(char *s, char *t);
 
 int main(){
 	char *str = "kawahara";
 	char *cpy;
 	cpy = (char*)malloc(sizeof(char)*10);
-	strcpy1(*cpy, *str);
+	strcpy1(cpy, str);
+	printf("%s\n", cpy);
 	free(cpy);
 }
 
-int strcpy1(char *cpy, char *org){
-	
+void strcpy1(char *cpy, char *org){
+	int len = strlen(org);
+	int i;
+	for (i = 0; i < len; ++i){
+		cpy[i] = org[i];
+	}
 }
