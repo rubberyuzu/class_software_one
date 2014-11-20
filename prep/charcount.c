6ca9e6d7b4792f8c2bf42bc384067af5a779404a
rubberyuzu <0696338342@mail.ecc.u-tokyo.ac.jp>
2014-11-20 15:58:11 +0900

    completed

--- prep/charcount.c
+++ prep/charcount.c
@@ -10,16 +10,34 @@ int main(){
 	// char string[9]="kawahara" ... OK
 	// char string[9]={'k','a','w','a','h','a','r','a','\0'}; ... OK
 	// char string[8]="kawahara" ... NG. needs to have null char.
+	printf("%s\n", "running");
 	int count[NALPHA][NALPHA];
-	int c;
+	int c, i, j;
 	int first;
 	int index = 0;
+	for( i = 0 ; i < NALPHA ; i++ ) {
+    for( j = 0 ; j < NALPHA ; j++ ) {
+      count[i][j] = 0;
+    }
+  }
+  	printf("%s\n", "running");
 	while((c=getchar())!=EOF){
 		if(index==0){
 			first = ' ';
+			index++;
 			continue;
 		}
-		count[first-'a'][c-'a']++;
+		if(('a'<=first&&first<'a'+NALPHA)&&
+			('a'<=c&&c<'a'+NALPHA)){
+			count[first-'a'][c-'a']++;
+		}
 		first = c;
+		index++;
 	}
+	printf("%s\n", "running");
+	for( i = 0 ; i < NALPHA ; i++ ) {
+    for( j = 0 ; j < NALPHA ; j++ ) {
+      printf("%c %c = %d\n",i+'a', j+'a', count[i][j] );
+    }
+  }
 }
